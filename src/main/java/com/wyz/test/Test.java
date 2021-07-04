5a77fe018a7e637598e70d3f90eb6baa40a3e6e1
wuyuzhen11 <970388658@qq.com>
2021-07-04 22:50:03 +0800

    first commit

---
+++ src/main/java/com/wyz/test/Test.java
@@ -0,0 +1,13 @@
+package com.wyz.test;
+
+/**
+ * @program: git-test
+ * @description:
+ * @author: wyz
+ * @create: 2021-07-04 22:46
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("11111111111111");
+    }
+}
