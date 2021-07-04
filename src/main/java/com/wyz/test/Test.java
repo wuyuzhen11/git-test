d61286e093ef4c95989797e09afa6dd823718158
wuyuzhen11 <970388658@qq.com>
2021-07-04 23:17:47 +0800

    push commit

--- src/main/java/com/wyz/test/Test.java
+++ src/main/java/com/wyz/test/Test.java
@@ -10,5 +10,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("11111111111111");
         System.out.println("111");
+        System.out.println("push test");
     }
 }
